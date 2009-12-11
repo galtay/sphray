1dda7305057c7f2091b430855523df7998503c6f
gabriel.altay <gabriel.altay@acc32f82-1241-11de-a236-ef2eabcd8114>
2009-12-11 20:43:22 +0000

    working on reading ionization tables

--- src/skewers.F90
+++ src/skewers.F90
@@ -5,95 +5,107 @@
 !<
 
 module skewers_mod
-use myf90_mod
-use main_input_mod, only: readin_snapshot
-use oct_tree_mod, only: buildtree, setparticleorder
-use raylist_mod, only: prepare_raysearch, kill_raylist, trace_ray
-use ray_mod, only: make_skewer_ray
-use ray_mod, only: ray_type, curface
-use b2cd_mod, only: b2cdfac
-use mt19937_mod, only: genrand_real1
-use output_mod, only: output_total_snap, ion_frac_out
-use physical_constants_mod
+  use myf90_mod
+  use main_input_mod, only: readin_snapshot
+  use oct_tree_mod, only: buildtree, setparticleorder
+  use raylist_mod, only: prepare_raysearch, kill_raylist, trace_ray
+  use ray_mod, only: make_skewer_ray
+  use ray_mod, only: ray_type, curface
+  use b2cd_mod, only: b2cdfac
+  use mt19937_mod, only: genrand_real1
+  use output_mod, only: output_total_snap, ion_frac_out
+  use physical_constants_mod
 
-! variables
-use global_mod, only: psys
-use global_mod, only: globalraylist
-use global_mod, only: tree
-use global_mod, only: GV, PLAN
-implicit none
+  ! variables
+  use global_mod, only: psys
+  use global_mod, only: globalraylist
+  use global_mod, only: tree
+  use global_mod, only: GV, PLAN
+  implicit none
+
+
+  integer, parameter :: Nwrite = 1000
+
 
 contains
 
 
-subroutine do_skewers()
-  character(clen), parameter :: myname="do_skewers"
-  logical, parameter :: crash=.true.
-  integer, parameter :: verb=1
-  character(clen) :: str,fmt
+  subroutine do_skewers()
+    character(clen), parameter :: myname="do_skewers"
+    logical, parameter :: crash=.true.
+    integer, parameter :: verb=1
+    character(clen) :: str,fmt
 
-  !  local counters 
-  !-----------------  
-  integer(i8b) :: snapn !< snapshot counter
-  integer(i8b) :: rayn  !< ray counter
-  integer(i8b) :: srcn  !< source counter
-  integer(i8b) :: i,j,k
-    
-  real(r8b) :: rn       !< random number
+    !  local counters 
+    !-----------------  
+    integer(i8b) :: snapn !< snapshot counter
+    integer(i8b) :: rayn  !< ray counter
+    integer(i8b) :: srcn  !< source counter
+    integer(i8b) :: i,j,k
 
-  integer(i8b) :: pindx
-  real(r8b) :: d,b,bnorm,cdfac
-  real(r8b) :: hsml,mass,rho,xHI
-  real(r8b) :: mass_cgs, rho_cgs, gpercm3, cm3
-  real(r8b) :: Hcnt, HIcnt, nH, NHI, NHItot
-  
-    
-  ! work variables
-  !-----------------
-  type(ray_type) :: ray
-  real(r8b) :: MB
+    real(r8b) :: rn       !< random number
+
+    integer(i8b) :: pindx
+    real(r8b) :: d,b,bnorm,cdfac
+    real(r8b) :: hsml,mass,rho,xHI
+    real(r8b) :: mass_cgs, rho_cgs, gpercm3, cm3
+    real(r8b) :: Hcnt, HIcnt, nH, NHI
+    real(r8b) :: NHItot(Nwrite), raypos(3,Nwrite)
+    integer(i8b) :: face(Nwrite), iwrite
 
 
-  ! loop over the snapshots 
-  !=========================
-  snaps: do snapn = GV%StartSnapNum, GV%EndSnapNum
-     
-
-     open(file="NHI.txt",unit=200)
 
 
-     call readin_snapshot(skewers=.true.)
-     
-     ! create custom src for skewering
-     !---------------------------------
-     if (allocated(psys%src) ) deallocate(psys%src)
-     allocate(psys%src(1))
-     
-     psys%src(1)%pos = (/0.0,0.0,0.0/)
-     psys%src(1)%EmisPrf = -3
-
-     !  build oct tree.  only need to do this once per snap (for now)
-     !----------------------------------------------------------------
-     call buildtree(psys,tree,MB,GV%PartPerCell)
-     GV%MB = GV%MB + MB
-     call setparticleorder(psys, tree)             
-     call prepare_raysearch(psys, globalraylist)
+    ! work variables
+    !-----------------
+    type(ray_type) :: ray
+    real(r8b) :: MB
 
 
-     GV%ForcedRayNumber = 60000
+    ! loop over the snapshots 
+    !=========================
+    snaps: do snapn = GV%StartSnapNum, GV%EndSnapNum
 
-     ! begin ray tracing 
-     !------------------------- 
-     src_rays: do rayn = 1, GV%ForcedRayNumber
 
-        if (mod(rayn,10000)==0) write(*,*) "rayn = ", rayn
-          
+       open(file=trim(GV%OutputDir)//"/NHI.txt",unit=200)
+
+
+       call readin_snapshot(skewers=.true.)
+
+       ! create custom src for skewering
+       !---------------------------------
+       if (allocated(psys%src) ) deallocate(psys%src)
+       allocate(psys%src(1))
+
+       psys%src(1)%pos = (/0.0,0.0,0.0/)
+       psys%src(1)%EmisPrf = -3
+
+       !  build oct tree.  only need to do this once per snap (for now)
+       !----------------------------------------------------------------
+       call buildtree(psys,tree,MB,GV%PartPerCell)
+       GV%MB = GV%MB + MB
+       call setparticleorder(psys, tree)             
+       call prepare_raysearch(psys, globalraylist)
+
+
+       GV%ForcedRayNumber = 60000
+       iwrite = 0
+
+       ! begin ray tracing 
+       !------------------------- 
+       src_rays: do rayn = 1, GV%ForcedRayNumber
+
+          if (mod(rayn,10000)==0) write(*,*) "rayn = ", rayn
+          iwrite = iwrite + 1
+          if (iwrite==Nwrite+1) iwrite=1
+
+
           GV%rayn = GV%rayn + 1
           GV%src_rayn = GV%src_rayn + 1
           GV%TotalSourceRaysCast = GV%TotalSourceRaysCast + 1      
-          
+
           curface=0
-       
+
           !  create a source ray and calc the impacts
           call make_skewer_ray(psys%box, ray)
 
@@ -101,9 +113,9 @@ subroutine do_skewers()
           globalraylist%ray = ray
           call trace_ray(globalraylist%ray, globalraylist, psys, tree) 
 
-          NHItot = 0.0d0
-          
-          100 format(A,I4,6F8.4, 2E14.5)
+          NHItot(iwrite) = 0.0d0
+
+100       format(A,I4,6F8.4, 2E14.5)
           do i = 1,globalraylist%nnb
 
              d = globalraylist%intersection(i)%d
@@ -120,23 +132,29 @@ subroutine do_skewers()
 
              mass_cgs = mass * GV%cgs_mass 
              rho_cgs  = rho *  GV%cgs_rho 
-             
+
              gpercm3 = rho_cgs
              cm3 = mass_cgs / rho_cgs
-             
+
              nH = rho_cgs * GV%H_mf  / M_H
              Hcnt = mass_cgs * GV%H_mf / M_H
              HIcnt = Hcnt * xHI
              NHI = cdfac * HIcnt
 
-             NHItot = NHItot + NHI
+             NHItot(iwrite) = NHItot(iwrite) + NHI
 
           end do
 
- 
-          write(200,"(I,4ES14.4)") curface, NHItot, ray%start
-
           
+          face(iwrite) = curface
+          raypos(1:3,iwrite) = ray%start(1:3)
+
+          if (mod(rayn,Nwrite)==0) then
+             do i = 1,Nwrite
+                write(200,"(I,4ES14.4)") face(i), NHItot(i), raypos(:,i)
+             end do
+          endif
+
 
 
        end do src_rays
@@ -146,10 +164,10 @@ subroutine do_skewers()
 
        stop
 
-  end do snaps
-  
-  
-end subroutine do_skewers
+    end do snaps
+
+
+  end subroutine do_skewers
 
 
 end module skewers_mod
