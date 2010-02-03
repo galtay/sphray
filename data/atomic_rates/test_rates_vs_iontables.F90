89be911e431c6a9a350b305910eb2cff6dd13fab
gabriel.altay <gabriel.altay@acc32f82-1241-11de-a236-ef2eabcd8114>
2010-02-03 23:39:53 +0000

    updated rate compare

--- data/atomic_rates/test_rates_vs_iontables.F90
+++ data/atomic_rates/test_rates_vs_iontables.F90
@@ -6,15 +6,16 @@ implicit none
 character(clen) :: rates_file
 character(clen) :: ion_file
 
-real :: z, logT, logD
+real :: z, logT, logD(2)
 real :: dlogT, logTmin, logTmax
-real :: x_i, x_h, x_c
-real*8 :: T, D
+real*8 :: x_i, x_hA, x_cA, x_hB, x_cB
+real*8 :: T, D(2)
 
-real :: RR, QQ, PP, det
-real :: GHI, CI, RC, y
-integer :: i, nsteps
+real*8 :: RR, QQ, PP, det
+real*8 :: GHI, CI, RC, y
+integer :: i, j, nsteps
 
+character(clen) :: files(2)
 
 type(ion_table_type) :: itable            !< CLOUDY ionization table
 
@@ -32,16 +33,19 @@ ion_file = "../ionization_tables/h1.hdf5"
 call read_atomic_rates_file(rtable, rates_file)
 call read_ion_table_file(ion_file, itable)
 
-
-z = 2.013
-logD = 1.0e-3
-D = 10**(logD)
+files(1) = "rate_comp_D1n5.txt"
+files(2) = "rate_comp_D1p2.txt"
 
 y=0.00
 GHI = itable%ihead%ispec%gammaHI(24)
+z = 2.013
 
 write(*,*) "z(24) = ", itable%z(24)
 write(*,*) "G(24) = ", GHI
+
+
+logD = (/ -5.0, 2.0 /)
+D = 10**(logD)
 
 
 logTmin=3.0
@@ -50,37 +54,53 @@ nsteps=100
 dlogT= (logTmax - logTmin)/(nsteps-1)
 
 
-open(unit=10, file="rate_comp.txt")
-do i = 0,nsteps-1
-   logT = logTmin + dlogT * i
-   T = 10**(logT)
-
-   x_i = interpolate_ion_table( itable, z, logT, logD )
 
 
-   call get_atomic_rates( T, rtable, k )
-   CI = Hui_HI_col_ion(T)
+do j = 1,2
 
-   RC = Hui_HII_recombA(T)
-   RR = -(CI + RC) * D
-   QQ = CI * D - GHI - (CI + RC) * D * y
-   PP = GHI + CI * D * y
-   det = QQ**2 - 4 * RR * PP
-   x_h = 1.0 - ( -QQ - sqrt(det) ) / (2*RR)
+   open(unit=10, file=files(j))
 
+   do i = 0,nsteps-1
+      logT = logTmin + dlogT * i
+      T = 10**(logT)
       
-   RC = Cen_HII_recombA(T)
-   RR = -(CI + RC) * D
-   QQ = CI * D - GHI - (CI + RC) * D * y
-   PP = GHI + CI * D * y
-   det = QQ**2 - 4 * RR * PP
-   x_c = 1.0 - ( -QQ - sqrt(det) ) / (2*RR)
+      x_i = interpolate_ion_table( itable, z, logT, logD(j) )
 
 
-   write(10,*) logT, x_h,x_c,x_i
+      call get_atomic_rates( T, rtable, k )
+      CI = Hui_HI_col_ion(T)
 
+      RC = Hui_HII_recombA(T)
+      RR = -(CI + RC) * D(j)
+      QQ = CI * D(j) - GHI - (CI + RC) * D(j) * y
+      PP = GHI + CI * D(j) * y
+      det = QQ**2 - 4 * RR * PP
+      x_hA = 1.0 - ( -QQ - sqrt(det) ) / (2*RR)
+      
+      
+      RC = Cen_HII_recombA(T)
+      RR = -(CI + RC) * D(j)
+      QQ = CI * D(j) - GHI - (CI + RC) * D(j) * y
+      PP = GHI + CI * D(j) * y
+      det = QQ**2 - 4 * RR * PP
+      x_cA = 1.0 - ( -QQ - sqrt(det) ) / (2*RR)
+      
+
+      RC = Hui_HII_recombB(T)
+      RR = -(CI + RC) * D(j)
+      QQ = CI * D(j) - GHI - (CI + RC) * D(j) * y
+      PP = GHI + CI * D(j) * y
+      det = QQ**2 - 4 * RR * PP
+      x_hB = 1.0 - ( -QQ - sqrt(det) ) / (2*RR)
+      
+      
+      write(10,"(5ES12.5)") logT, x_hA, x_cA, x_i, x_hB
+
+   end do
+   close(10)
+   
 end do
-close(10)
+
 
 
 
