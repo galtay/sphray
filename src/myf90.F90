44318c1e0c1141aa8f7d970675c1eef3c049bd2a
gabriel.altay <gabriel.altay@acc32f82-1241-11de-a236-ef2eabcd8114>
2010-03-14 01:54:26 +0000

    updated file scanner

--- src/myf90.F90
+++ src/myf90.F90
@@ -37,16 +37,6 @@ implicit none
   integer, parameter :: r8b  = selected_real_kind(p=15,r=307)  !< 8 byte real type
   integer, parameter :: r16b = selected_real_kind(p=33,r=4931) !< 16 byte real type
 
-! set calculation precision
-!------------------------
-  integer, parameter :: rcp = r8b
-  integer, parameter :: icp = i8b
-
-! set output precision
-!------------------------
-  integer, parameter :: rop = r4b
-  integer, parameter :: iop = i4b
-
 
   integer, parameter :: clen  = 500 !< default character variable length
 
@@ -319,63 +309,44 @@ contains
 
   ! scan file routines, one for each type
   !==================================================================
-  function tmp_file_name(nchars) result(file)
-    integer :: nchars
-    character(nchars) :: file
-    
-    integer :: i, ticks, seedsize
-    integer, allocatable :: seed(:)
-    real :: x
-    
-    call random_seed(size=seedsize)
-    allocate( seed(seedsize) )
-    do i = 1,seedsize
-       call system_clock(count=ticks)
-       seed(i)=ticks
-    end do
-    call random_seed(put=seed)
-    
-    file="a"
-    do i = 1,nchars-1
-       call random_number(x)
-       write(file,"(A,I1)") trim(file), int(x*10)
-    end do
-    
-  end function tmp_file_name
-
-
   subroutine scanfile_r4b(filename,keyword,var) 
     character(*), intent(in) :: filename
     character(*), intent(in) :: keyword
     real(r4b), intent(out) :: var
-    character(26) :: tmpfile 
-    character(clen) :: cmnd
 
-    integer(i4b) :: lun, Nfound
-    real :: rn
-    integer :: i, c
+    integer :: keylen
+    integer :: strlen
+    integer :: keypos
+    character(clen) :: string
+    integer :: ierr
+    integer :: lun
+    integer :: count
 
-    tmpfile = tmp_file_name(len(tmpfile))
-
-    ! produce awk command and send it to the system
-    !-----------------------------------------------
-    100 format("awk '{if ($1 ~ /^",A,"$/) print $2}' ",A, " > ",A)
-    write(cmnd,100) trim(keyword), trim(filename), tmpfile
-    call system(cmnd)
-    
-    Nfound = scanfile_count_args(tmpfile,filename,keyword)
-
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,*) var
+    count=0
+    keylen = len(adjustl(trim(keyword)))
+    call open_formatted_file_r(filename,lun)
+    do while( ierr==0 ) 
+       read(lun,'(A)',iostat=ierr) string
+       strlen = len(adjustl(trim(string)))
+       keypos = index( string, adjustl(trim(keyword)) )
+       if (keypos == 1) then
+          count = count + 1
+          read(string(keypos+keylen:strlen),*) var
+       endif
+    end do
     close(lun)
 
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
+    if (count/=1) then
+       write(*,'(A,A)') ' keyword ', trim(keyword)
+       write(*,'(A,A)') ' file:   ', trim(filename)
+       if (count==0) then
+          write(*,*) ' *** keyword not found in file'
+          stop
+       else if (count>1) then
+          write(*,*) ' *** keyword fount ', count, ' times'
+          stop
+       endif
+    endif
 
   end subroutine scanfile_r4b
 
@@ -384,32 +355,40 @@ contains
     character(*), intent(in) :: filename
     character(*), intent(in) :: keyword
     real(r8b), intent(out) :: var
-    character(26) :: tmpfile 
-    character(clen) :: cmnd
 
-    integer(i4b) :: lun, Nfound
-    
-    tmpfile = tmp_file_name(len(tmpfile))
+    integer :: keylen
+    integer :: strlen
+    integer :: keypos
+    character(clen) :: string
+    integer :: ierr
+    integer :: lun
+    integer :: count
 
-    ! produce awk command and send it to the system
-    !-----------------------------------------------
-    100 format("awk '{if ($1 ~ /^",A,"$/) print $2}' ",A, " > ",A)
-    write(cmnd,100) trim(keyword), trim(filename), tmpfile
-    call system(cmnd)
-
-    Nfound = scanfile_count_args(tmpfile,filename,keyword)
-    
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,*) var
+    count=0
+    keylen = len(adjustl(trim(keyword)))
+    call open_formatted_file_r(filename,lun)
+    do while( ierr==0 ) 
+       read(lun,'(A)',iostat=ierr) string
+       strlen = len(adjustl(trim(string)))
+       keypos = index( string, adjustl(trim(keyword)) )
+       if (keypos == 1) then
+          count = count + 1
+          read(string(keypos+keylen:strlen),*) var
+       endif
+    end do
     close(lun)
 
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
+    if (count/=1) then
+       write(*,'(A,A)') ' keyword ', trim(keyword)
+       write(*,'(A,A)') ' file:   ', trim(filename)
+       if (count==0) then
+          write(*,*) ' *** keyword not found in file'
+          stop
+       else if (count>1) then
+          write(*,*) ' *** keyword fount ', count, ' times'
+          stop
+       endif
+    endif
 
   end subroutine scanfile_r8b
 
@@ -418,32 +397,40 @@ contains
     character(*), intent(in) :: filename
     character(*), intent(in) :: keyword
     integer(i4b), intent(out) :: var
-    character(26) :: tmpfile 
-    character(clen) :: cmnd
 
-    integer(i4b) :: lun, Nfound
-        
-    tmpfile = tmp_file_name(len(tmpfile))
+    integer :: keylen
+    integer :: strlen
+    integer :: keypos
+    character(clen) :: string
+    integer :: ierr
+    integer :: lun
+    integer :: count
 
-    ! produce awk command and send it to the system
-    !-----------------------------------------------
-    100 format("awk '{if ($1 ~ /^",A,"$/) print $2}' ",A, " > ",A)
-    write(cmnd,100) trim(keyword), trim(filename), tmpfile
-    call system(cmnd)
-    
-    Nfound = scanfile_count_args(tmpfile,filename,keyword)
-
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,*) var
+    count=0
+    keylen = len(adjustl(trim(keyword)))
+    call open_formatted_file_r(filename,lun)
+    do while( ierr==0 ) 
+       read(lun,'(A)',iostat=ierr) string
+       strlen = len(adjustl(trim(string)))
+       keypos = index( string, adjustl(trim(keyword)) )
+       if (keypos == 1) then
+          count = count + 1
+          read(string(keypos+keylen:strlen),*) var
+       endif
+    end do
     close(lun)
 
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
+    if (count/=1) then
+       write(*,'(A,A)') ' keyword ', trim(keyword)
+       write(*,'(A,A)') ' file:   ', trim(filename)
+       if (count==0) then
+          write(*,*) ' *** keyword not found in file'
+          stop
+       else if (count>1) then
+          write(*,*) ' *** keyword fount ', count, ' times'
+          stop
+       endif
+    endif
 
   end subroutine scanfile_i4b
 
@@ -453,32 +440,40 @@ contains
     character(*), intent(in) :: filename
     character(*), intent(in) :: keyword
     integer(i8b), intent(out) :: var
-    character(26) :: tmpfile 
-    character(clen) :: cmnd
-    
-    integer(i4b) :: lun, Nfound
-    
-    tmpfile = tmp_file_name(len(tmpfile))
-    
-    ! produce awk command and send it to the system
-    !-----------------------------------------------
-    100 format("awk '{if ($1 ~ /^",A,"$/) print $2}' ",A, " > ",A)
-    write(cmnd,100) trim(keyword), trim(filename), tmpfile
-    call system(cmnd)
-    
-    Nfound = scanfile_count_args(tmpfile,filename,keyword)
 
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,*) var
+    integer :: keylen
+    integer :: strlen
+    integer :: keypos
+    character(clen) :: string
+    integer :: ierr
+    integer :: lun
+    integer :: count
+
+    count=0
+    keylen = len(adjustl(trim(keyword)))
+    call open_formatted_file_r(filename,lun)
+    do while( ierr==0 ) 
+       read(lun,'(A)',iostat=ierr) string
+       strlen = len(adjustl(trim(string)))
+       keypos = index( string, adjustl(trim(keyword)) )
+       if (keypos == 1) then
+          count = count + 1
+          read(string(keypos+keylen:strlen),*) var
+       endif
+    end do
     close(lun)
 
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
+    if (count/=1) then
+       write(*,'(A,A)') ' keyword ', trim(keyword)
+       write(*,'(A,A)') ' file:   ', trim(filename)
+       if (count==0) then
+          write(*,*) ' *** keyword not found in file'
+          stop
+       else if (count>1) then
+          write(*,*) ' *** keyword fount ', count, ' times'
+          stop
+       endif
+    endif
 
   end subroutine scanfile_i8b
 
@@ -488,33 +483,41 @@ contains
     character(*), intent(in) :: filename
     character(*), intent(in) :: keyword
     character(clen), intent(out) :: var
-    character(26) :: tmpfile 
-    character(clen) :: cmnd
 
-    integer(i4b) :: lun, Nfound    
-    
-    tmpfile = tmp_file_name(len(tmpfile))
+    integer :: keylen
+    integer :: strlen
+    integer :: keypos
+    character(clen) :: string
+    integer :: ierr
+    integer :: lun
+    integer :: count
 
-    ! produce awk command and send it to the system
-    !-----------------------------------------------
-    100 format("awk '{if ($1 ~ /^",A,"$/) print $2}' ",A, " > ",A)
-    write(cmnd,100) trim(keyword), trim(filename), tmpfile
-    call system(cmnd)
-    
-    Nfound = scanfile_count_args(tmpfile,filename,keyword)
-
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,'(A)') var
+    count=0
+    keylen = len(adjustl(trim(keyword)))
+    call open_formatted_file_r(filename,lun)
+    do while( ierr==0 ) 
+       read(lun,'(A)',iostat=ierr) string
+       strlen = len(adjustl(trim(string)))
+       keypos = index( string, adjustl(trim(keyword)) )
+       if (keypos == 1) then
+          count = count + 1
+          read(string(keypos+keylen:strlen),'(A)') var
+          var = adjustl(trim(var))
+       endif
+    end do
     close(lun)
 
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
-
+    if (count/=1) then
+       write(*,'(A,A)') ' keyword ', trim(keyword)
+       write(*,'(A,A)') ' file:   ', trim(filename)
+       if (count==0) then
+          write(*,*) ' *** keyword not found in file'
+          stop
+       else if (count>1) then
+          write(*,*) ' *** keyword fount ', count, ' times'
+          stop
+       endif
+    endif
   end subroutine scanfile_chr
 
 
@@ -523,83 +526,45 @@ contains
     character(*), intent(in) :: filename
     character(*), intent(in) :: keyword
     logical, intent(out) :: var
-    character(26) :: tmpfile 
-    character(clen) :: cmnd
 
-    integer(i4b) :: lun, Nfound
-        
-    tmpfile = tmp_file_name(len(tmpfile))
+    integer :: keylen
+    integer :: strlen
+    integer :: keypos
+    character(clen) :: string
+    integer :: ierr
+    integer :: lun
+    integer :: count
 
-    ! produce awk command and send it to the system
-    !-----------------------------------------------
-    100 format("awk '{if ($1 ~ /^",A,"$/) print $2}' ",A, " > ",A)
-
-    write(cmnd,100) trim(keyword), trim(filename), tmpfile
-    call system(cmnd)
-    
-    Nfound = scanfile_count_args(tmpfile,filename,keyword)
-
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,*) var
+    count=0
+    keylen = len(adjustl(trim(keyword)))
+    call open_formatted_file_r(filename,lun)
+    do while( ierr==0 ) 
+       read(lun,'(A)',iostat=ierr) string
+       strlen = len(adjustl(trim(string)))
+       keypos = index( string, adjustl(trim(keyword)) )
+       if (keypos == 1) then
+          count = count + 1
+          read(string(keypos+keylen:strlen),*) var
+       endif
+    end do
     close(lun)
 
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
+    if (count/=1) then
+       write(*,'(A,A)') ' keyword ', trim(keyword)
+       write(*,'(A,A)') ' file:   ', trim(filename)
+       if (count==0) then
+          write(*,*) ' *** keyword not found in file'
+          stop
+       else if (count>1) then
+          write(*,*) ' *** keyword fount ', count, ' times'
+          stop
+       endif
+    endif
 
   end subroutine scanfile_log
 
 
-  ! retunrs the number of matchs awk found
-  function scanfile_count_args(awkoutfile,paramfile,keyword) result(N)
-    character(*), intent(in) :: awkoutfile
-    character(*), intent(in) :: paramfile
-    character(*), intent(in) :: keyword
-    integer(i4b) :: N
-    character(27) :: tmpfile 
-    character(clen) :: cmnd
-    integer(i4b) :: lun
 
-    tmpfile = tmp_file_name(len(tmpfile))
- 
-    100 format("awk 'END {print NR}' ",A, " > ",A)
-    write(cmnd,100) awkoutfile, tmpfile
-    call system(cmnd)
-
-    ! read variable from temporary file
-    !-----------------------------------
-    call open_formatted_file_r(tmpfile,lun)
-    read(lun,*) N
-    close(lun)
-
-    ! remove temporary file
-    !-----------------------
-    200 format("rm -f ", A)
-    write(cmnd,200) tmpfile
-    call system(cmnd)
-
-    if (N==1) return
-
-    write(*,*) " *** error while parsing file ", trim(paramfile)
-        
-    if (N==0) then
-       write(*,*) "keyword ", trim(keyword), " does not appear in file"
-    else if (N > 1) then
-       write(*,*) "keyword ", trim(keyword), " is defined ", N , " times"
-    end if
-
-    ! remove other temporary file
-    !-----------------------------
-    write(cmnd,200) awkoutfile
-    call system(cmnd)
-
-    stop
-
-  end function scanfile_count_args
 
 
 end module myf90_mod
